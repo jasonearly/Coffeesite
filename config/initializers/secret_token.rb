eb126e0c88c1c4bf95e8348306617373b086ce0e
Jason Early <theoffice@gruntmonkey.com>
2013-05-09 19:03:04 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeSite::Application.config.secret_token = 'ad09e393cd03e8a79376acac8da8a2b2baa9c9e18f62902e5b68eabc52b189202a507cd2ba137d37362788c032933ec578c08b0f888bee65a8da675e9f2eb6ca'
