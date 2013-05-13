b565a3328c439748dc7b6daac0a9c3dff41e7869
Jason Early <theoffice@gruntmonkey.com>
2013-05-13 11:44:39 -0500

    seeded test users into db, and added authorization

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,10 +1,12 @@
 class ContentController < ApplicationController
-  def silver
-  end
-
-  def gold
-  end
-
-  def platinum
-  end
+	before_filter :authenticate_user!
+		def silver
+		authorize! :view, :silver, :message => 'Access limited to Silver Plan subscribers.'
+	end
+		def gold
+		authorize! :view, :gold, :message => 'Access limited to Gold Plan subscribers.'
+	end
+		def platinum
+		authorize! :view, :platinum, :message => 'Access limited to Platinum Plan subscribers.'
+	end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,14 @@
 class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.has_role? :admin
-      can :manage, :all
-    end
+    include CanCan::Ability
+        def initialize(user)
+            user ||= User.new # guest user (not logged in)
+        if user.has_role? :admin
+            can :manage, :all
+        else
+            can :view, :silver if user.has_role? :silver
+            can :view, :gold if user.has_role? :gold
+            can :view, :platinum if user.has_role? :platinum
+        end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
