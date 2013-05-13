1124be2f4d99ef12c41597531ea075d3a20a5e1c
Jason Early <theoffice@gruntmonkey.com>
2013-05-13 15:26:11 -0500

    add stripe

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
-def new
-@plan = params[:plan]
-if @plan && ENV["ROLES"].include?(@plan) && @plan != "admin"
-super
-else
-redirect_to root_path, :notice => 'Please select a subscription plan below.'
-end
-end
+
+  def new
+    @plan = params[:plan]
+    if @plan && ENV["ROLES"].include?(@plan) && @plan != "admin"
+      super
+    else
+      redirect_to root_path, :notice => 'Please select a subscription plan below.'
+    end
+  end
 private
-def build_resource(*args)
-super
-if params[:plan]
-resource.add_role(params[:plan])
-end
-end
+  def build_resource(*args)
+    super
+    if params[:plan]
+      resource.add_role(params[:plan])
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,53 @@
 class User < ActiveRecord::Base
 	rolify
-	# Include default devise modules. Others available are:
-	# :token_authenticatable, :confirmable,
-	# :lockable, :timeoutable and :omniauthable
-	devise :database_authenticatable, :registerable,
-	:recoverable, :rememberable, :trackable, :validatable
-
-	# Setup accessible (or protected) attributes for your model
-	attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-
+# Include default devise modules. Others available are:
+# :token_authenticatable, :confirmable,
+# :lockable, :timeoutable and :omniauthable
+devise :database_authenticatable, :registerable,
+:recoverable, :rememberable, :trackable, :validatable
+# Setup accessible (or protected) attributes for your model
+attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :stripe_token, :coupon
+attr_accessor :stripe_token, :coupon
+before_save :update_stripe
+def update_stripe
+	return if email.include?(ENV['ADMIN_EMAIL'])
+	return if email.include?('@example.com') and not Rails.env.production?
+	if customer_id.nil?
+		if !stripe_token.present?
+			raise "Stripe token not present. Can't create account."
+		end
+		if coupon.blank?
+			customer = Stripe::Customer.create(
+				:email => email,
+				:description => name,
+				:card => stripe_token,
+				:plan => roles.first.name
+				)
+		else
+			customer = Stripe::Customer.create(
+				:email => email,
+				:description => name,
+				:card => stripe_token,
+				:plan => roles.first.name,
+				:coupon => coupon
+				)
+		end
+	else
+		customer = Stripe::Customer.retrieve(customer_id)
+		if stripe_token.present?
+			customer.card = stripe_token
+		end
+		customer.email = email
+		customer.description = name
+		customer.save
+	end
+	self.last_4_digits = customer.active_card.last4
+	self.customer_id = customer.id
+	self.stripe_token = nil
+rescue Stripe::StripeError => e
+	logger.error "Stripe Error: " + e.message
+	errors.add :base, "#{e.message}."
+	self.stripe_token = nil
+	false
+end
 end
