7d0b402f4ae49fd6adcd22152ddd4c8751241093
Jason Early <theoffice@gruntmonkey.com>
2013-05-13 17:00:07 -0500

    accommodate changes to account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,24 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
     @users = User.all
   end
-
   def show
     @user = User.find(params[:id])
   end
-  
   def update
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user], :as => :admin)
+    role = Role.find(params[:user][:role_ids]) unless params[:user][:role_ids].nil?
+    params[:user] = params[:user].except(:role_ids)
+    if @user.update_attributes(params[:user])
+      @user.update_plan(role) unless role.nil?
       redirect_to users_path, :notice => "User updated."
     else
       redirect_to users_path, :alert => "Unable to update user."
     end
   end
-    
   def destroy
     authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
     user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 CoffeeSite::Application.routes.draw do
-  get "content/silver"
-
-  get "content/gold"
-
-  get "content/platinum"
-
-  authenticated :user do
-    root :to => 'home#index'
-  end
-  root :to => "home#index"
-  devise_for :users, :controllers => { :registrations => 'registrations' }
-  resources :users
+	get "content/gold"
+	get "content/silver"
+	get "content/platinum"
+	authenticated :user do
+		root :to => 'home#index'
+	end
+	root :to => "home#index"
+	devise_for :users, :controllers => { :registrations => 'registrations' }
+	devise_scope :user do
+		put 'update_plan', :to => 'registrations#update_plan'
+		put 'update_card', :to => 'registrations#update_card'
+	end
+	resources :users
 end
